a19b4cc43cef84bf6ae0c1aea8a5f856b53edfcd
Li Jie <lijie@applean.cn>
2017-10-20 16:46:10 +0800

    Finish react-navigation demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { View, Text, TouchableOpacity } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+
+class Main extends Component {
+  static navigationOptions = {
+    title: 'Main'
+  }
+
+  goHelp = () => {
+    this.props.navigation.navigate('Help')
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Main page</Text>
+        <TouchableOpacity onPress={this.goHelp}>
+          <Text>Go to help</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+class Help extends Component {
+  static navigationOptions = {
+    title: 'Help'
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Help page</Text>
+      </View>
+    )
+  }
+}
+
+const HelpStack = StackNavigator({
+  Help: { screen: Help }
+}, {
+  headerMode: 'none'
+})
+
+const Nav = StackNavigator({
+  Main: { screen: Main },
+  Help: { screen: HelpStack }
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <View style={{width: 327, height: 578, borderWidth: 1, borderColor: 'black', overflow: 'hidden', margin: 30}}>
+        <Nav />
+      </View>
     );
   }
 }
